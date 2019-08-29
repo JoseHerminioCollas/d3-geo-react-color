71bc3f5f95c6a0e580c80820f22085a20e06ddf6
Jose Collas <joseherminiocollas@goatstonc.com>
2019-08-29 11:11:58 -0700

    add info panel

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,42 @@ const App: React.FC = () => {
   const { cssSheet }: any = useContext(ThemeContext)
   return (
     <section className={cssSheet.classes.mainContainer}>
-      <i
-        className="material-icons"
-        style={{ display: 'none' }}
-      >
-        face
-      </i>
       <Control />
       <ColorGlobe
         Map={Map}
       />
+      <section
+        style={{
+          color: '#111',
+          position: 'fixed',
+          bottom: '3px',
+          left: '3px',
+        }}
+      >
+        <a
+          target="new"
+          href="https://github.com/JoseHerminioCollas/d3-geo-react-color"
+        >
+          <i
+            className="material-icons"
+          >
+            info
+          </i>
+        </a>
+        &nbsp;&nbsp;
+        ColorGlobe
+        <a
+          style={{
+            textDecoration: 'none',
+            fontSize: '0.9em',
+          }}
+          target="new"
+          href="http://goatstone.com"
+        >
+          &nbsp;&nbsp;
+          Goatstone &copy; 2019
+        </a>
+      </section>
     </section>
   )
 }
